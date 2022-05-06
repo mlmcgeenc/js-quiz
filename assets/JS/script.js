1c4ea386f87222994ec461317908314c88e00e97
Matt McGee <mlmcgeenc@gmail.com>
2022-05-06 15:41:58 -0400

    add start quiz function

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,28 @@
-timeLeftEl = document.getElementById("time-left")
-var timeLeft = 0;
+timeLeftEl = document.getElementById("time-left");
+titleEl = document.getElementById("title");
+contentEl = document.getElementById("content");
+startButtonEl = document.querySelector("#start-button");
+var initialTime = 0;
 
-timeLeftEl.textContent = timeLeft;
+timeLeftEl.textContent = initialTime;
+
+var handleQuizStart = function () {
+	startTimer();
+	titleEl.textContent = "This is a placeholder for the first question.";
+	contentEl.innerHTML = "<ol><li>item</li><li>item</li><li>item</li><li>item</li></ol>";
+};
+
+var startTimer = function () {
+	var timeLeft = 60;
+	var timeInterval = setInterval(function () {
+		if (timeLeft > 0) {
+			timeLeftEl.textContent = timeLeft;
+            timeLeft--
+		} else {
+			timeLeftEl.textContent = "GAME OVER";
+			clearInterval(timeInterval);
+		}
+	}, 1000);
+};
+
+startButtonEl.addEventListener("click", handleQuizStart);
