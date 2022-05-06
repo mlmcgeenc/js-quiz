a9694aa5e651e0f794d7857e1d2bfbda9c14dfdb
Matt McGee <mlmcgeenc@gmail.com>
2022-05-06 15:51:07 -0400

    create array of question objects

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,6 +4,49 @@ contentEl = document.getElementById("content");
 startButtonEl = document.querySelector("#start-button");
 var initialTime = 0;
 
+var questions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answer1: "strings",
+        answer2: "booleans",
+        answer3: "alerts",
+        answer4: "numbers",
+        correctAnswerIndex: 3,
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____.",
+        answer1: "quotes",
+        answer2: "curly brackets",
+        answer3: "parenthesis",
+        answer4: "square brackets",
+        correctAnswerIndex: 2,
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        answer1: "numbers and strings",
+        answer2: "other arrays",
+        answer3: "booleans",
+        answer4: "all of the above",
+        correctAnswerIndex: 3,
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        answer1: "commas",
+        answer2: "curly brackets",
+        answer3: "quotes",
+        answer4: "parenthesis",
+        correctAnswerIndex: 2,
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer1: "JavaScript",
+        answer2: "terminal/bash",
+        answer3: "for loops",
+        answer4: "console.log",
+        correctAnswerIndex: 3,
+    },
+]
+
 timeLeftEl.textContent = initialTime;
 
 var handleQuizStart = function () {
